69362f83caa652effc4fdbba46f9bd5018584bdd
Tariel Arslanov <tarielarslanov5@gmail.com>
2023-04-01 00:51:57 +0300

    добавил роут для несуществующих маршрутов

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="*" element={<Home/>} />
         <Route path="/services" element={<Services/>} />
         <Route path="/widgets" element={<Widgets/>} />
         <Route path="/integrations" element={<Integrations/>} />
